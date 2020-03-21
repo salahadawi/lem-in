4b17904c324ad6f1cdf0fe974162fee93136abd5
sadawi <sadawi.hahli144@hotmail.com>
2020-03-21 19:50:54 +0200

    add faster file reading

--- lem-in.c
+++ lem-in.c
@@ -6,17 +6,19 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 17:18:53 by sadawi            #+#    #+#             */
-/*   Updated: 2020/01/24 18:31:47 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/03/21 18:26:30 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/lem-in.h"
 
-int		handle_error(int error_code)
+int		handle_error(int code)
 {
-	if (error_code)
+	if (code == 1)
 		ft_printf("Error\n");
-	return (1);
+	else if (code == 2)
+		ft_printf("Error: Malloc failed\n");
+	exit(0);
 }
 
 int		check_ants_amount(char *line)
@@ -112,6 +114,48 @@ int		check_file(char **file)
 	return (0);
 }
 
+char	*ft_strcatnl(char *s1, const char *s2)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	if (!s2)
+		return (s1);
+	while (s1[i])
+		i++;
+	while (s2[j])
+	{
+		s1[i + j] = s2[j];
+		j++;
+	}
+	s1[i + j] = '\n';
+	s1[i + j + 1] = '\0';
+	return (s1);
+}
+
+char	*join_input(char *s1, char *s2)
+{
+	char *str;
+	int i;
+	int j;
+
+	i = -1;
+	j = -1;
+	if (!(str = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 2)))
+		return (NULL);
+	while (s1[++i])
+		str[i] = s1[i];
+	while (s2[++j])
+		str[i + j] = s2[j];
+	str[i + j] = '\n';
+	str[i + j + 1] = '\0';
+	free(s1);
+	free(s2);
+	return (str);
+}
+
 char	**save_input(void)
 {
 	char **file;
@@ -119,10 +163,10 @@ char	**save_input(void)
 	char *line;
 
 	input = ft_strnew(0);
-	while (get_next_line(0, &line))
+	while (get_next_line(0, &line) > 0)
 	{
-		line = ft_strjoinfree(line, ft_strdup("\n"));
-		input = ft_strjoinfree(input, line);
+		if (!(input = join_input(input, line)))
+			handle_error(2);
 	}
 	file = ft_strsplit(input, '\n');
 	free(input);
@@ -136,10 +180,12 @@ int	main(void)
 
 	i = 0;
 	file = save_input();
-	if (!check_file(file))
-		return (handle_error(1));
+	//if (!check_file(file))
+		//return (handle_error(1));
 	while (file[i])
+	{
 		ft_printf("%s\n", file[i++]);
+	}
 	return (0);
 }
 
