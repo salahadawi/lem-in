9521efbf7138228394d1b2ba08dd619f284dcc69
sadawi <sadawi.hahli144@hotmail.com>
2020-03-22 13:27:18 +0200

    fix memory leak, split code

--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: sadawi <sadawi@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/23 17:18:53 by sadawi            #+#    #+#             */
-/*   Updated: 2020/03/21 21:15:53 by sadawi           ###   ########.fr       */
+/*   Updated: 2020/03/22 13:26:46 by sadawi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 int		handle_error(int code)
 {
 	if (code == 1)
-		ft_printf("Error\n");
+		ft_printf("Error: File is invalid.\n");
 	else if (code == 2)
-		ft_printf("Error: Malloc failed\n");
+		ft_printf("Error: Malloc failed.\n");
 	exit(0);
 }
 
@@ -114,53 +114,6 @@ int		check_file(char **file)
 	return (0);
 }
 
-char	*ft_strcatnl(char *s1, const char *s2)
-{
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	if (!s2)
-		return (s1);
-	while (s1[i])
-		i++;
-	while (s2[j])
-	{
-		s1[i + j] = s2[j];
-		j++;
-	}
-	s1[i + j] = '\n';
-	s1[i + j + 1] = '\0';
-	return (s1);
-}
-
-char	*join_input(char *s1, char *s2)
-{
-	char *str;
-	int i;
-	int j;
-
-	i = -1;
-	j = -1;
-	if (!(str = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 2)))
-		return (NULL);
-	while (s1[++i])
-		str[i] = s1[i];
-	while (s2[++j])
-		str[i + j] = s2[j];
-	str[i + j] = '\n';
-	str[i + j + 1] = '\0';
-	free(s1);
-	free(s2);
-	return (str);
-}
-
-void	file_append(t_file *file, t_file **new)
-{
-	file->next = *new;
-}
-
 t_file	*file_new(char *line)
 {
 	t_file *file;
@@ -168,21 +121,46 @@ t_file	*file_new(char *line)
 	if (!(file = (t_file*)ft_memalloc(sizeof(t_file))))
 		handle_error(2);
 	file->next = NULL;
-	if (!(file->line = ft_strnew(ft_strlen(line))))
-		handle_error(2);
-	file->line = ft_strdup(line);
+	file->line = line;
 	return (file);
 }
 
-t_file	*save_input(int *size)
+void	free_and_go_to_next(t_file **file)
+{
+	t_file *prev;
+	
+	prev = *file;
+	*file = (*file)->next;
+	free(prev);
+}
+
+char	**list_to_arr(t_file *file, int size)
+{
+	char	**new_file;
+	int		i;
+	
+	if (!(new_file = (char**)ft_memalloc(sizeof(char**) * (size + 1))))
+		handle_error(2);
+	i = 0;
+	while (file)
+	{
+		new_file[i++] = file->line;
+		free_and_go_to_next(&file);
+	}
+	new_file[i] = 0;
+	return (new_file);
+}
+
+char	**save_input(void)
 {
 	char *line;
 	t_file *first;
 	t_file *tmp;
+	int		size;
 	
 	first = NULL;
 	tmp = NULL;
-	*size = 0;
+	size = 0;
 	while (get_next_line(0, &line) > 0)
 	{
 		if (tmp)
@@ -194,49 +172,45 @@ t_file	*save_input(int *size)
 			tmp = file_new(line);
 		if (!first)
 			first = tmp;
-		(*size)++;
+		size++;
 	}
-	ft_printf("%d", *size);
 	tmp->next = NULL;
-	return (first);
+	return (list_to_arr(first, size));
 }
 
-char	**list_to_arr(t_file *file, int size)
+void	print_file(char **file)
 {
-	char	**new_file;
-	int		i;
-	
-	if (!(new_file = (char**)malloc(sizeof(char*) * (size + 1))))
-		handle_error(2);
+	int i;
+
 	i = 0;
-	while (file)
-	{
-		new_file[i++] = file->line;
-		file = file->next;
-	}
-	new_file[i] = 0;
-	return (new_file);
+	while (file[i])
+		ft_printf("%s\n", file[i++]);
+}
+
+void	free_file(char **file)
+{
+	int i;
+
+	i = 0;
+	while (file[i])
+		free(file[i++]);
+	free(file[i]);
+	free(file);
 }
 
 int	main(void)
 {
-	t_file	*file;
-	char	**file2;
-	int		size;
-	int		i;
+	char	**file;
 	
-	file = save_input(&size);
+	file = save_input();
 	//if (!check_file(file))
 		//return (handle_error(1));
-	file2 = list_to_arr(file, size);
-	i = 0;
-	while (file2[i])
-	{
-		ft_printf("%s\n", file2[i]);
-		i++;
-	}
+	print_file(file);
+	free_file(file);
 	return (0);
 }
 
 //Need to check if room exists
 //Need to check if input contains empty lines and return error
+//Remember to free file after everything
+//Maybe should learn more about optimal file reading and parsing?
