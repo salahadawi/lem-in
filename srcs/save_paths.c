b07d10c9117ce965031d12c82ad171fea8779c99
sadawi <hahli144@hotmail.com>
2020-07-24 16:03:39 +0300

    mergesort paths into order by size

--- srcs/save_paths.c
+++ srcs/save_paths.c
@@ -544,6 +544,63 @@ t_path	*get_paths(t_farm **farm)
 	return (path_head);
 }
 
+t_path	*sorted_merge(t_path *first_half, t_path *second_half)
+{
+	t_path *sorted;
+
+	if (!first_half)
+		return (second_half);
+	if (!second_half)
+		return (first_half);
+	if (first_half->size < second_half->size)
+	{
+		sorted = first_half;
+		sorted->next = sorted_merge(first_half->next, second_half);
+	}
+	else
+	{
+		sorted = second_half;
+		sorted->next = sorted_merge(first_half, second_half->next);
+	}
+	return (sorted);
+}
+
+void	split_list(t_path *head, t_path **first_half, t_path **second_half)
+{
+	t_path *fast;
+	t_path *slow;
+
+	slow = head;
+	fast = head->next;
+	while (fast)
+	{
+		fast = fast->next;
+		if (fast)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+	}
+	*first_half = head;
+	*second_half = slow->next;
+	slow->next = NULL;
+}
+
+void	mergesort_paths(t_path **paths)
+{
+	t_path	*head;
+	t_path	*first_half;
+	t_path	*second_half;
+
+	head = *paths;
+	if (!head || !head->next)
+		return ;
+	split_list(head, &first_half, &second_half);
+	mergesort_paths(&first_half);
+	mergesort_paths(&second_half);
+	*paths = sorted_merge(first_half, second_half);
+}
+
 void	save_paths(t_farm **farm)
 {
 	t_path	*final_paths;
@@ -553,8 +610,11 @@ void	save_paths(t_farm **farm)
 	//print_paths(*farm, (*farm)->paths);
 	//print_flow(farm, NULL);
 	final_paths = get_paths(farm);
-	//print_paths(*farm, final_paths);
+	print_paths(*farm, final_paths);
+	ft_printf("\n\n");
 	(*farm)->paths = final_paths;
+	mergesort_paths(&(*farm)->paths);
+	print_paths(*farm, (*farm)->paths);
 }
 
 //clean up everything, free memory
